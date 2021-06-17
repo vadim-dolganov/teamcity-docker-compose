25ed37ae25e49d5baab9057bf1d7b881012a2502
root <root@teamcity-server-sre.ispring.lan>
2021-06-17 15:59:12 +0300

    refactoring

--- postgres/restore.sh
+++ postgres/restore.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-
-# stop on errors
 set -e
-
-# we might run into trouble when using the default `postgres` user, e.g. when dropping the postgres
-# database in restore.sh. Check that something else is used here
 if [ "$POSTGRES_USER" == "postgres" ]
 then
     echo "restoring as the postgres user is not supported, make sure to set the POSTGRES_USER environment variable"
     exit 1
 fi
 
-# export the postgres password so that subsequent commands don't ask for it
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-# check that we have an argument for a filename candidate
 if [[ $# -eq 0 ]] ; then
     echo 'usage:'
     echo '    docker-compose run postgres restore <backup-file>'
@@ -24,10 +17,8 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-# set the backupfile variable
 BACKUPFILE=/backups/$1
 
-# check that the file exists
 if ! [ -f $BACKUPFILE ]; then
     echo "backup file not found"
     echo 'to get a list of available backups, run:'
